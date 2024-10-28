c61c6afe47aeb90c67bd7571187f18f06361a2ad
Ganapathy <ganapathy@testpress.in>
2024-10-28 17:38:47 +0530

    Building a chat server for e learning project
        create a chat app install needed packages and build ASGI applica
    -tion to handling real time updates use Redis for channel layer and
    intergrate with the existing views.

--- educa/settings.py
+++ educa/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'embed_video',
     'memcache_status',
     'rest_framework',
+    'chat',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -149,3 +151,15 @@ REST_FRAMEWORK={
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+ASGI_APPLICATION='educa.routing.application'
+
+CHANNEL_LAYERS={
+    'default':{
+        'BACKEND':'channels_redis.core.RedisChannelLayer',
+        'CONFIG':{
+            'hosts':[('127.0.0.1',6379)],
+        }
+    }
+}
+
